772ff780d4627d7c661448dc70eebf6c5b9c1a46
WhipFalcon <tripletwo567@gmail.com>
2017-07-26 10:18:43 +0300

    Fix Typo

--- about_methods.rb
+++ about_methods.rb
@@ -1,36 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-def my_global_method(a,b)
+def my_global_method(a, b)
   a + b
 end
 
 class AboutMethods < Neo::Koan
-
   def test_calling_global_methods
-    assert_equal 5, my_global_method(2,3)
+    assert_equal 5, my_global_method(2, 3)
   end
 
-def test_calling_global_methods_without_parentheses
+  def test_calling_global_methods_without_parentheses
     result = my_global_method 2, 3
     assert_equal 5, result
-  end
+    end
 
   # (NOTE: We are Using eval below because the example code is
   # considered to be syntactically invalid).
   def test_sometimes_missing_parentheses_are_ambiguous
-      eval "assert_equal 5, my_global_method(2, 3)"
-      # (eval):1: warning: don't put space before argument parentheses
-      # eval "assert_equal 5, my_global_method (2, 3)"
-    # eval "assert_equal 5, my_global_method 2, 3" # ENABLE CHECK
-    #
-    # Ruby doesn't know if you mean:
-    #
-    #   assert_equal(5, my_global_method(2), 3)
-    # or
-    #   assert_equal(5, my_global_method(2, 3))
-    #
-    # Rewrite the eval string to continue.
-    #
+    eval 'assert_equal 5, my_global_method(2, 3)'
+  # (eval):1: warning: don't put space before argument parentheses
+  # eval "assert_equal 5, my_global_method (2, 3)"
+  # eval "assert_equal 5, my_global_method 2, 3" # ENABLE CHECK
+  #
+  # Ruby doesn't know if you mean:
+  #
+  #   assert_equal(5, my_global_method(2), 3)
+  # or
+  #   assert_equal(5, my_global_method(2, 3))
+  #
+  # Rewrite the eval string to continue.
+  #
 end
 
   # NOTE: wrong number of arguments is not a SYNTAX error, but a
@@ -42,14 +41,14 @@ end
     assert_match(/wrong number of arguments/, exception.message)
 
     exception = assert_raise(ArgumentError) do
-      my_global_method(1,2,3)
+      my_global_method(1, 2, 3)
     end
     assert_match(/wrong number of arguments/, exception.message)
   end
 
   # ------------------------------------------------------------------
 
-  def method_with_defaults(a, b=:default_value)
+  def method_with_defaults(a, b = :default_value)
     [a, b]
   end
 
@@ -68,7 +67,7 @@ end
     assert_equal Array, method_with_var_args.class
     assert_equal [], method_with_var_args
     assert_equal [:one], method_with_var_args(:one)
-    assert_equal [:one,:two], method_with_var_args(:one, :two)
+    assert_equal %i[one two], method_with_var_args(:one, :two)
   end
 
   # ------------------------------------------------------------------
@@ -101,22 +100,22 @@ end
   end
 
   def test_calling_methods_in_same_class
-    assert_equal 12, my_method_in_the_same_class(3,4)
+    assert_equal 12, my_method_in_the_same_class(3, 4)
   end
 
   def test_calling_methods_in_same_class_with_explicit_receiver
-    assert_equal 12, self.my_method_in_the_same_class(3,4)
+    assert_equal 12, my_method_in_the_same_class(3, 4)
   end
 
   # ------------------------------------------------------------------
 
   def my_private_method
-    "a secret"
+    'a secret'
   end
   private :my_private_method
 
   def test_calling_private_methods_without_receiver
-    assert_equal "a secret", my_private_method
+    assert_equal 'a secret', my_private_method
   end
 
   def test_calling_private_methods_with_an_explicit_receiver
@@ -130,19 +129,19 @@ end
 
   class Dog
     def name
-      "Fido"
+      'Fido'
     end
 
     private
 
     def tail
-      "tail"
+      'tail'
     end
   end
 
   def test_calling_methods_in_other_objects_require_explicit_receiver
     rover = Dog.new
-    assert_equal "Fido", rover.name
+    assert_equal 'Fido', rover.name
   end
 
   def test_calling_private_methods_in_other_objects
--- triangle.rb
+++ triangle.rb
@@ -14,19 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  
   s = (a + b + c) / 2.0
-  
+
   # the following must be positive to be a valid triangle
   ok = (s - a) * (s - b) * (s - c)
-  
-  if a <= 0 || b <= 0 || c <= 0 || ok <= 0 then 
-    raise TriangleError
-  end
-  
-  if a == b && b == c then
+
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0 || ok <= 0
+
+  if a == b && b == c
     :equilateral
-  elsif a == b || a == c || b == c then
+  elsif a == b || a == c || b == c
     :isosceles
   else
     :scalene
